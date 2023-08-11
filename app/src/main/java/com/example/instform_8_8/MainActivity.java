d51174f15d7d0f3486669a393f93e662c7c64f39
shahd-alrahbi <138566899+shahd-alrahbi@users.noreply.github.com>
2023-08-11 14:03:55 +0400

    Initial commit

---
+++ app/src/main/java/com/example/instform_8_8/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.instform_8_8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "instForm_8_8"
+include(":app")
+ 
